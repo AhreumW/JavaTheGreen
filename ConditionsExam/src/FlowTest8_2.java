8517b3335e884373aa58954fe5d8d556fa6ac1ac
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-13 09:50:09 +0900

    수정

--- ConditionsExam/src/FlowTest8_2.java
+++ ConditionsExam/src/FlowTest8_2.java
@@ -22,20 +22,30 @@ public class FlowTest8_2 {
 		System.out.println("해당 월을 입력해주세요.");
 		month = scan.nextInt();
 		boolean check = true; 
-		
+	
 		switch(month) {
 			case 12 : case 1: case 2: 
-				System.out.println("");
+				season = "겨울";
 				break;
 			case 3 : case 4: case 5: 
-				System.out.println("");
+				season = "봄";
 				break;
 			case 6 : case 7: case 8: 
-				System.out.println("");
+				season = "여름";
 				break;	
 			case 9 : case 10: case 11: 
-				System.out.println("");
+				season = "가을";
 				break;	
+			default :
+				check = false;
+				break;
+		}
+			
+		if(check == true) {
+			System.out.println(month+"월은 "+season+"입니다.");
+			
+		}else {
+			System.out.println("월은 1월부터 12월까지 입니다.");
 		}
 		
 		
--- ConditionsExam/src/RockPaperScissorsMain.java
+++ ConditionsExam/src/RockPaperScissorsMain.java
@@ -9,6 +9,7 @@ public class RockPaperScissorsMain {
 
 	public static void main(String[] args) {
 		
+		
 		int user = 0;
 		int com = 0;
 
@@ -22,17 +23,45 @@ public class RockPaperScissorsMain {
 		com = (int)(Math.random() * 3 ) + 1 ;
 		System.out.println("컴퓨터도 선택했습니다.");
 		
+		String userChoiceStr = "";
+		switch (user) {
+			case 1:
+				userChoiceStr = "가위";
+				break;
+			case 2:
+				userChoiceStr = "바위";
+				break;
+			case 3:
+				userChoiceStr = "보";
+				break;
+			default:
+				break;
+		}
+		String comChoiceStr = "";
+		switch (com) {
+			case 1:
+				comChoiceStr = "가위";
+				break;
+			case 2:
+				comChoiceStr = "바위";
+				break;
+			case 3:
+				comChoiceStr = "보";
+				break;
+			default:
+				break;
+		}
 		
-		System.out.println("당신은" + user + "입니다.");
-		System.out.println("com은"+ com + "입니다.");
+		System.out.println("당신은 " + userChoiceStr + "입니다.");
+		System.out.println("com은 "+ comChoiceStr + "입니다.");
 		
 		String msgstr = ""; 
 				
 		switch(user - com) {
-			case 2: case -1:
+			case 2: case -1:			// 1-2 ,2-3 ,3-1
 				msgstr = "당신이 졌습니다.";
 				break;
-			case 1: case -2:
+			case 1: case -2:			// 1-3 ,2-1 ,3-2
 				msgstr = "당신이 이겼습니다.";
 				break;
 			case 0:
