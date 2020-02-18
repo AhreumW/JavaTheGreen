078483714188b593ab4ab63ee60e8f3f10746bfe
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-18 18:51:54 +0900

    클래스와 객체

--- Objects6/src/CardMain.java
+++ Objects6/src/CardMain.java
@@ -28,7 +28,7 @@ public class CardMain {
 		
 		// 클래스 변수는 인스턴스를 생성하지 않고도 언제라도 바로 사용할 수 있다
 		//'클래스이름.클래스변수'
-		//Card.width = 70;
+		//Card.width = 70; 
 		
 		System.out.println("card1은 "+ card1.kind + ", "+ card1.num + "이며, "
 				+ "크기는 "+ card1.width + "*"+ card1.height);
