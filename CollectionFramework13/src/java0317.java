f0880d9454141ea94e62df45c5cc068d43b3118e
TJ <TJ@DESKTOP-J2LI200>
2020-03-17 10:47:28 +0900

    컬렉션프레임워크

---
+++ CollectionFramework13/src/java0317.java
@@ -0,0 +1,18 @@
+
+public class java0317 {
+
+	public static void main(String[] args) {
+		
+		
+		int[] numArr = new int[10];
+		
+		for(int i =0; i< numArr.length; i++) {
+			numArr[i] = i+1;
+		}
+		
+		for(int i =0; i< numArr.length; i++) {
+			System.out.println("numArr["+i+"] : "+ numArr[i]);
+		}
+		
+	}
+}
